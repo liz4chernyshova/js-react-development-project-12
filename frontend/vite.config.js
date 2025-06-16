b7169f84776688804c5b2a5486e4bddadd5fb3e4
Liza Chernyshova <liz4chernyshova@yandex.ru>
2025-06-16 09:51:37 +0300

    fix lint errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5001'
-    }
+      '/api': 'http://localhost:5001',
+    },
   },
   plugins: [react()],
 })
